9955f159a05ae564eb3e3a886f5b18231ce28456
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2019-06-15 01:33:46 +0800

    finished q1

--- TMA1Question1.cpp
+++ TMA1Question1.cpp
@@ -20,10 +20,6 @@
 
     Compile: clang++ TMAQuestion1.cpp -o TMAQuestion1
     Execution: ./TMAQuestion1 <file1> <file2> <file...>
-
-    Classes: 
-        1. WordCounter: a class to open the file and count the words.
-
 */
 #include <string>
 #include <iostream>
@@ -31,20 +27,6 @@
 #include <vector>
 
 using namespace std;
-
-
-
-class WordCounter
-{
-    private:
-        string s;          // temp string to store words
-        ifstream file;     // file holder
-        int counter;       // words counter
-    public:
-        WordCounter(const char *__s);    // constructor for this class
-        int numWords();                  // return the number of words in file
-        void count();                    // do the counting
-};
 
 
 
@@ -56,79 +38,29 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
-    // ifstream file;
-    // string s;
-    //
-    // file.open(*(argv + 1));
-    // if (!file.good())
-    // {
-    //     clog << "error: no such file or directory: " << *(argv + 1) << endl;
-    //     return 1;
-    // }
-    //
-    // cout << "file read."<< endl;
-    //
-    // int counter = -1;
-    // while (file >> s)
-    // {
-    //     counter++;
-    // }
-    //
-    // cout << "word count: " << counter << endl;
-
-    vector<WordCounter> list;       // vector for the sake of flexibility
+    ifstream file;
+    string s;
+    
     for (int i = 1; i < argc; i++)
-    // for every filenames given in the argument list
     {
-        try
+        file.open(*(argv + i));
+        
+        if (!file.good())
         {
-            WordCounter *temp = new WordCounter(*(argv + i));
-            list.push_back(*temp);
-        }
-        catch(string e)
-        {
-            clog << "error: " << e << ". skip." << endl;
+            clog << "error: no such file or directory: " << *(argv + i) << endl;
             continue;
         }
+        
+        int counter = 0;
+        while (file >> s)
+        {
+            counter++;
+        }
+        
+        cout << "File \"" << *(argv + i) << "\" ";
+        cout << "word count: " << counter << endl;
+        file.close();
     }
     
-    for (int i = 0; i < list.size(); i++)
-    {
-        list[i].count();
-        cout << "word count: " << list[i].numWords() << endl;
-    }
-
     return 0;
-}
-
-
-
-// constructor
-WordCounter::WordCounter(const char *__s) 
-{
-    this-> file.open(__s);
-    this-> counter = 0;
-    if (!this-> file.good())
-    {
-        string e;
-        e = "no such file or directory: ";
-        e += __s;
-        throw e;
-    }
-    this-> counter = -1;
-    cout << "file read."<< endl;
-}
-
-void WordCounter::count()
-{
-    this-> counter = 0;
-    while (this-> file >> this-> s)
-    {
-        counter++;
-    }
-}
-
-int WordCounter::numWords()
-{
-    return this-> counter;
 }
---
+++ test.sh
@@ -0,0 +1,3 @@
+### Q1 Test ###
+./TMA1Question1 normal.txt void.txt space_only.txt non_english_script.txt
+
