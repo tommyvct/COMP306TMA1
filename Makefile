8bba0bd650ef2c4f3b2b687d01f6bf7c0743f74c
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2019-06-23 17:21:25 +0800

    finished!

--- Makefile
+++ Makefile
@@ -1,36 +1,52 @@
 CC = clang++
 CFLAGS = -Wall
 DEBUG = -g
+SRC = ./src
+BIN = ./bin
+
+
+TMA1: BIN TMA1Q1 TMA1Q2 TMA1Q3 TMA1Q4 TMA1Q5
+
+debug: BIN TMA1Q1_debug TMA1Q2_debug TMA1Q3_debug TMA1Q4_debug TMA1Q5_debug
+
+BIN: 
+	mkdir bin
 
 
 
-TMA1: TMA1Q1 TMA1Q2 TMA1Q3
+TMA1Q1: $(SRC)/TMA1Question1.cpp
+	$(CC) $(CFLAGS) $(SRC)/TMA1Question1.cpp -o $(BIN)/TMA1Question1
 
-debug:TMA1Q1_debug TMA1Q2_debug TMA1Q3_debug
+TMA1Q2: $(SRC)/TMA1Question2.cpp
+	$(CC) $(CFLAGS) $(SRC)/TMA1Question2.cpp -o $(BIN)/TMA1Question2
+
+TMA1Q3: $(SRC)/TMA1Question3.cpp
+	$(CC) $(CFLAGS) $(SRC)/TMA1Question3.cpp -o $(BIN)/TMA1Question3
+
+TMA1Q4: $(SRC)/TMA1Question4.cpp
+	$(CC) $(CFLAGS) $(SRC)/TMA1Question4.cpp -o $(BIN)/TMA1Question4
+
+TMA1Q5: $(SRC)/TMA1Question5.cpp
+	$(CC) $(CFLAGS) $(SRC)/TMA1Question5.cpp -o $(BIN)/TMA1Question5
 
 
 
-TMA1Q1: TMA1Question1.cpp
-	$(CC) $(CFLAGS) TMA1Question1.cpp -o TMA1Question1
+TMA1Q1_debug: $(SRC)/TMA1Question1.cpp
+	$(CC) $(CFLAGS) $(DEBUG) $(SRC)/TMA1Question1.cpp -o $(BIN)/TMA1Question1
 
-TMA1Q2: TMA1Question2.cpp
-	$(CC) $(CFLAGS) TMA1Question2.cpp -o TMA1Question2
+TMA1Q2_debug: $(SRC)/TMA1Question2.cpp
+	$(CC) $(CFLAGS) $(DEBUG) $(SRC)/TMA1Question2.cpp -o $(BIN)/TMA1Question2
 
-TMA1Q3: TMA1Question3.cpp
-	$(CC) $(CFLAGS) TMA1Question3.cpp -o TMA1Question3
+TMA1Q3_debug: $(SRC)/TMA1Question3.cpp
+	$(CC) $(CFLAGS) $(DEBUG) $(SRC)/TMA1Question3.cpp -o $(BIN)/TMA1Question3
 
+TMA1Q4_debug: $(SRC)/TMA1Question4.cpp
+	$(CC) $(CFLAGS) $(DEBUG) $(SRC)/TMA1Question4.cpp -o $(BIN)/TMA1Question4
 
-
-TMA1Q1_debug: TMA1Question1.cpp
-	$(CC) $(CFLAGS) $(DEBUG) TMA1Question1.cpp -o TMA1Question1
-
-TMA1Q2_debug: TMA1Question2.cpp
-	$(CC) $(CFLAGS) $(DEBUG) TMA1Question2.cpp -o TMA1Question2
-
-TMA1Q3_debug: TMA1Question3.cpp
-	$(CC) $(CFLAGS) $(DEBUG) TMA1Question3.cpp -o TMA1Question3
+TMA1Q5_debug: $(SRC)/TMA1Question5.cpp
+	$(CC) $(CFLAGS) $(DEBUG) $(SRC)/TMA1Question5.cpp -o $(BIN)/TMA1Question5
 
 
 
 clean:
-	rm TMA1Question1 TMA1Question2 TMA1Question3
+	rm -rf $(BIN)
--- test.sh
+++ test.sh
@@ -2,19 +2,19 @@ make
 
 echo "### Q1 Test ###"
 echo "nornal case:"
-./TMA1Question1 ./Q1Test/GoodFile1.txt ./Q1Test/GoodFile2.txt
+./bin/TMA1Question1 ./TMA1Test/GoodFile1.txt ./TMA1Test/GoodFile2.txt
 echo "Bad case 1:"
-./TMA1Question1 ./Q1Test/GoodFile1.txt ./Q1Test/nonexist.txt
+./bin/TMA1Question1 ./TMA1Test/GoodFile1.txt ./TMA1Test/nonexist.txt
 echo "Bad case 2:"
-./TMA1Question1 ./Q1Test/nonexist.txt ./Q1Test/GoodFile1.txt
+./bin/TMA1Question1 ./TMA1Test/nonexist.txt ./TMA1Test/GoodFile1.txt
 echo "Bad case 3:"
-./TMA1Question1 ./Q1Test/nonexist.txt ./Q1Test/ZeroLength.txt
+./bin/TMA1Question1 ./TMA1Test/nonexist.txt ./TMA1Test/ZeroLength.txt
 echo "Bad case 4:"
-./TMA1Question1
+./bin/TMA1Question1
 echo "Bad case 5:"
-./TMA1Question1 ./Q1Test/nonEnglishScript.txt
+./bin/TMA1Question1 ./TMA1Test/nonEnglishScript.txt
 echo "Bad case 6:" 
-./TMA1Question1 ./Q1Test/SpaceOnly.txt
+./bin/TMA1Question1 ./TMA1Test/SpaceOnly.txt
 
 echo
 echo
@@ -22,18 +22,60 @@ echo
 
 echo "### Q2 Test ###"
 echo "nornal case:"
-./TMA1Question2 ./Q1Test/GoodFile1.txt ./Q1Test/GoodFile2.txt
+./bin/TMA1Question2 ./TMA1Test/GoodFile1.txt ./TMA1Test/GoodFile2.txt
 echo "Bad case 1:"
-./TMA1Question2 ./Q1Test/GoodFile1.txt ./Q1Test/nonexist.txt
+./bin/TMA1Question2 ./TMA1Test/GoodFile1.txt ./TMA1Test/nonexist.txt
 echo "Bad case 2:"
-./TMA1Question2 ./Q1Test/nonexist.txt ./Q1Test/GoodFile1.txt
+./bin/TMA1Question2 ./TMA1Test/nonexist.txt ./TMA1Test/GoodFile1.txt
 echo "Bad case 3:"
-./TMA1Question2 ./Q1Test/nonexist.txt ./Q1Test/ZeroLength.txt
+./bin/TMA1Question2 ./TMA1Test/nonexist.txt ./TMA1Test/ZeroLength.txt
 echo "Bad case 4:"
-./TMA1Question2
+./bin/TMA1Question2
 echo "Bad case 5:"
-./TMA1Question2 ./Q1Test/nonEnglishScript.txt
+./bin/TMA1Question2 ./TMA1Test/nonEnglishScript.txt
 echo "Bad case 6:" 
-./TMA1Question2 ./Q1Test/SpaceOnly.txt
+./bin/TMA1Question2 ./TMA1Test/SpaceOnly.txt
+
+echo
+echo
+echo
+
+echo "### Q3 Test ###"
+./bin/TMA1Question3 
+
+echo
+echo
+echo
+
+echo "### Q4 Test ###"
+echo "normal case 1:"
+./bin/TMA1Question4
+echo "normal case 2:"
+./bin/TMA1Question4 123456
+echo "bad case 1:"
+./bin/TMA1Question4 fsdkkfjhsdfh
+echo "bad case 2:"
+./bin/TMA1Question4 -6666
+
+echo
+echo
+echo
+
+echo "### Q5 Test ###"
+echo "normal case:"
+./bin/TMA1Question5 ./TMA1Test/GoodFile1.txt ./TMA1Test/GoodFile2.txt
+echo "Bad case 1:"
+./bin/TMA1Question5 ./TMA1Test/GoodFile1.txt ./TMA1Test/nonexist.txt
+echo "Bad case 2:"
+./bin/TMA1Question5 ./TMA1Test/nonexist.txt ./TMA1Test/GoodFile1.txt
+echo "Bad case 3:"
+./bin/TMA1Question5 ./TMA1Test/SpaceOnly.txt ./TMA1Test/ZeroLength.txt
+echo "Bad case 4:"
+./bin/TMA1Question5
+echo "Bad case 5:"
+./bin/TMA1Question5 ./TMA1Test/NonEnglishScript.txt
+echo "Bad case 6:" 
+./bin/TMA1Question5 ./TMA1Test/SpaceOnly.txt
+
 
 make clean
